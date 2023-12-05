a716ff36453cfdcc98f30e121075d76a6742f1ff
Deepanjan Nag <subscriptions@deepanjannag.com>
2023-12-04 22:28:35 -0500

    Add project files.

---
+++ EFCore.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,52 @@
+﻿using EFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Data
+{
+    public class FootballLeagueDbContext : DbContext
+    //dotnet add package Microsoft.EntityFrameworkCore --version 7 FROM THIS FOLDER's COMMAND PROMPT
+    //dotnet tool install --global dotnet-ef --version 7
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=FootballLeague_EFCore;Trusted_Connection=True;TrustServerCertificate=True").LogTo(Console.WriteLine, LogLevel.Information).EnableSensitiveDataLogging().EnableDetailedErrors();
+        }
+
+        //Needed only for Seeding
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasData(
+                new Team
+                {
+                    Id = 1,
+                    Name = "Tivoli Gardens F.C.",
+                    DateCreated = DateTimeOffset.UtcNow.DateTime
+                },
+                    new Team
+                    {
+                        Id = 2,
+                        Name = "Waterhouse F.C.",
+                        DateCreated = DateTimeOffset.UtcNow.DateTime
+                    },
+                    new Team
+                    {
+                        Id = 3,
+                        Name = "Humble Lions F.C.",
+                        DateCreated = DateTimeOffset.UtcNow.DateTime
+                    }
+                ) ;
+
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EFCore.Data/Migrations/20231205022558_ReSeededTeams.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReSeededTeams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "Name" },
+                values: new object[] { new DateTime(2023, 12, 5, 2, 25, 58, 753, DateTimeKind.Unspecified).AddTicks(3872), "Tivoli Gardens F.C." });
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "Name" },
+                values: new object[] { new DateTime(2023, 12, 5, 2, 25, 58, 753, DateTimeKind.Unspecified).AddTicks(3880), "Waterhouse F.C." });
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "Name" },
+                values: new object[] { new DateTime(2023, 12, 5, 2, 25, 58, 753, DateTimeKind.Unspecified).AddTicks(3881), "Humble Lions F.C." });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "Name" },
+                values: new object[] { new DateTime(2023, 12, 5, 2, 17, 33, 372, DateTimeKind.Unspecified).AddTicks(3179), "Mohan Bagan" });
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "Name" },
+                values: new object[] { new DateTime(2023, 12, 5, 2, 17, 33, 372, DateTimeKind.Unspecified).AddTicks(3185), "East Bengal" });
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "Name" },
+                values: new object[] { new DateTime(2023, 12, 5, 2, 17, 33, 372, DateTimeKind.Unspecified).AddTicks(3187), "Inter Milan" });
+        }
+    }
+}
